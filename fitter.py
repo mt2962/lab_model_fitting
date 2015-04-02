07b586f7b4e26912d09c68fe014b24f52f52e79a
Aleksey <aleksey.generozov@gmail.com>
2015-04-02 14:20:36 -0400

    changing things to be consistent with hubble law.

--- fitter.py
+++ fitter.py
@@ -2,37 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 #  ------------------------------------------------------
 #  PLOTDATA: plots a line and saves
 #  it as a png image ...
 #
 def plotData(x,y,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.title('Raw Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.title('Raw Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  PLOTFIT: plots line AND data and saves it as a png
 #
 #
 def plotFit(x,y,z,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.plot(x,z,'k-')
-	plt.title('Fit to Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.plot(x,z,'k-')
+    plt.title('Fit to Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  GRABDATA
 #    Pulls data from file and stores it in x,y
 #
 def grabData():
-	dat=np.genfromtxt('linfit.txt')
-	x=dat[:,0]
-	y=dat[:,1]
+    dat=np.genfromtxt('linfit.txt')
+    x=dat[:,0]
+    y=dat[:,1]
 
-	return (x,y)
+    return (x,y)
 
 
 
@@ -40,8 +45,8 @@ def grabData():
 #  F
 #    Given data list x, returns a list y = mx+b 
 #
-def f(x,m,b):
-	return 0					# FIXME!
+def hubble(x,h0):
+    return 0.# FIXME!
 
 #
 #   SSR
@@ -51,39 +56,37 @@ def f(x,m,b):
 #     by calculating the sum of the square of
 #     the residuals
 #
-def SSR(x,y,m,b):
-	return 0 					# FIXME!
+def SSR(x,y,h0):
+
 
 #
 #  STEP 3 ---- Get data, plot it
 #
 
+x, y=grabData()
 
-#
 #  STEP 5 ---- Plot example function
 #
+
 
 #
 #  STEP 7 ---- Find SSR for poor fit from example function
 #
+
 
 #
 #  STEP 8 ---- Make lists for possible slopes and
 #              intercepts, then print them
 #
 
-slopes=np.arange(0.,100.,1.)
-bs=np.arange(0.,100.,1.)    
-
+h0s=np.arange(0.,100.,0.1)
 
 #
 #  STEP 9 ---- Find slope and intercept that minimize
 #              the RSS
 #
-bestSlope = 0
-bestInter = 0
+bestSlope = 0.
 min_ssr = np.inf
-
 
 #
 #  STEP 10 ---- Plot Result
@@ -95,3 +98,4 @@ min_ssr = np.inf
 #  STEP 11 ---- Plot Residuals
 #
 #
+
