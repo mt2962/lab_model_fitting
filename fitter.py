199369f60396cc4963c527c518e79443c23f34ab
mt2962 <mt2962@columbia.edu>
2015-04-16 22:05:23 +0000

    finished

--- fitter.py
+++ fitter.py
@@ -1,10 +1,11 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 
 #  ------------------------------------------------------
@@ -33,7 +34,7 @@ def plotFit(x,y,z,name):
 #    Pulls data from file and stores it in x,y
 #
 def grabData():
-    dat=np.genfromtxt('linfit.txt')
+    dat=np.genfromtxt('linFit.txt')
     x=dat[:,0]
     y=dat[:,1]
 
@@ -44,7 +45,15 @@ def grabData():
 #  ------------------------------------------------------
 # Step 3: Given list of x return list of h0*x
 def hubble(x,h0):
-    return 0.
+
+    m= [] 
+    for i in x:
+         m.append(h0*i)
+    
+    return m
+    
+        
+        
 
 
 #     Given data lists x and y, this function
@@ -55,21 +64,42 @@ def hubble(x,h0):
 
 #Step 5:
 def SSR(x,y,h0):
+    b=hubble(x,h0)
+    
+    SSR=0
+    for i in range(len(x)):
 
+        r = b[i]-y[i]
+        rsq=r**2
+        SSR=SSR+rsq
+        
+    return SSR 
+    
+        
 
 #
 #  Step 2: Get data, plot it
 #
 
 x, y=grabData()
+plotData(x,y,'rawData.png')
+
 
 #   Step 4: Plot example function
 #
-
+x,y=grabData()
+z= hubble(x,10)
+plotFit(x,y,z,'badFit.png')
 
 #
 #  Step 6: Find SSR for poor fit from example function
 #
+
+x,y=grabData()
+
+m = SSR(x,y,10)
+print m
+
 
 
 #
@@ -85,14 +115,39 @@ h0s=np.arange(0.,100.,0.1)
 best_h0 = 0.
 min_ssr = np.inf
 
+for h0 in h0s: 
+
+    k = SSR(x,y,h0)
+    if k<min_ssr:
+        best_h0=h0
+        min_ssr=k
+
+
+print best_h0
+print min_ssr
+
+    
+
+
+    
+
 #
 #   Step 8: Plot Result
 #
 #
-
+x,y=grabData()
+z=hubble(x,best_h0)
+plotFit(x,y,z,'bestFit.png')
 
 #
 #   Step 9: Plot Residuals
 #
 #
+res =[z[i]-y[i] for i in range(len(y))]
+
+plotData(x,res,'resids.png')
+
+
+
+
 
